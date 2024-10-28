b5d22ff5e38431177d42ce6485751799304070c8
lestday233 <106459514+lest-day@users.noreply.github.com>
2024-10-29 06:49:15 +0800

    Update constants.ts

--- mobile/src/modules/i18n/constants.ts
+++ mobile/src/modules/i18n/constants.ts
@@ -2,6 +2,7 @@
 export const LANGUAGES = [
   { code: "de", name: "Deutsch" },
   { code: "en", name: "English" },
+  { code: "cn", name: "简体中文" },
   { code: "id", name: "Indonesia" },
   { code: "ja", name: "日本語" },
 ] as const;

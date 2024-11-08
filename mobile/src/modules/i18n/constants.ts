0e46d16bd95339532433be5ba191379b27bccdba
lestday233 <106459514+lest-day@users.noreply.github.com>
2024-11-09 00:35:38 +0800

    更新 constants.ts

--- mobile/src/modules/i18n/constants.ts
+++ mobile/src/modules/i18n/constants.ts
@@ -1,8 +1,8 @@
 /** List of available languages that our app supports. */
 export const LANGUAGES = [
+  { code: "cn", name: "简体中文" },
   { code: "de", name: "Deutsch" },
   { code: "en", name: "English" },
-  { code: "cn", name: "简体中文" },
   { code: "id", name: "Indonesia" },
   { code: "ja", name: "日本語" },
 ] as const;
